20035ccab9354541f437ddc483c60c172d63724f
VIKAS <developervikas17@gmail.com>
2018-01-05 09:15:13 +0530

    nothing

--- contactlist/app.js
+++ contactlist/app.js
@@ -24,14 +24,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api',route);
 
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use('/api',route);
+
+
 app.get('/',(req,res)=>{
     res.send("hllo vikas");
 
-})
+});
+
 app.listen(port,()=>{
     console.log("server started as port: "+port);
 });
